9c6360092840c04e3c3c517d610d8a25cf3badf3
jacek <mendyk.ja@gmail.com>
2021-09-30 20:31:35 +0200

    adding 'bullet.py' file

---
+++ bullet.py
@@ -0,0 +1,30 @@
+import pygame
+from pygame.sprite import Sprite
+
+
+class Bullet(Sprite):
+    """Class for managing bullets delivered by spaceship"""
+
+    def __init__(self, ai_game):
+        """Creating bullet object in actual spaceship's location"""
+        super().__init__()
+        self.screen = ai_game.screen
+        self.settings = ai_game.settings
+        self.color = self.settings.bullet_color
+
+        # Creating a bullet square in  (0, 0) location and then
+        # Defining the right location for it
+        self.rect = pygame.Rect(0, 0, self.settings.bullet_width, self.settings.bullet_height)
+        self.rect.midtop = ai_game.ship.rect.midtop
+
+        # Bullet location is defined by float value
+        self.y = float(self.rect.y)
+
+    def update(self):
+        """Moving bullet on the screen"""
+        # Location actualization of bullet square
+        self.rect.y = self.y
+
+    def draw_bullet(self):
+        """ Displaying bullet on the screen"""
+        pygame.draw.rect(self.screen, self.color, self.rect)
--- settings.py
+++ settings.py
@@ -7,5 +7,10 @@ class Settings():
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (230,  230, 230)
-        # Settings that concern spaceship.
+        # Settings concerning spaceship.
         self.ship_speed = 1.5
+        # Settings concerning bullet.
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
